6de42ee85521c3ae56f6032cbf9faae54a1eee67
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-08-10 18:53:17 -0300

    Mejora estetica inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./inicio.css"
+import { ClientesContext } from '../../context/ClientesContext'
 
 const Inicio = () => {
+  const { cantClientes } = useContext(ClientesContext)
+
   return (
     <>
       <h1 className="text-center mt-4">Bienvenido!</h1>
       <div className="container-inicio">
-        <div className="container-datos bg-primary">
-          <p>dato1</p>
+        <div className="container-datos">
+          <h2 className="text-center mb-4">Clientes</h2>
+          <h3 className="text-center">{cantClientes}</h3>
         </div>
-        <div className="container-datos bg-primary">
-          <p>dato2</p>
+        <div className="container-datos">
+          <h2 className="text-center mb-4">Gnancia</h2>
+          <h3 className="text-center">$40.980</h3>
         </div>
-        <div className="container-datos bg-primary">
-          <p>dato3</p>
+        <div className="container-datos">
+          <h2 className="text-center mb-4">dato3</h2>
+          <h3 className="text-center">dato3</h3>
         </div>
       </div>
     </>
