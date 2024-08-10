4d88ce8d89d7c7895e42ac6b3612fbb0f5a78a70
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-08-10 16:26:56 -0300

    Preparacion de pantalla de inicio para posterior carga de datos

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./inicio.css"
 
 const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <>
+      <h1 className="text-center mt-4">Bienvenido!</h1>
+      <div className="container-inicio">
+        <div className="container-datos bg-primary">
+          <p>dato1</p>
+        </div>
+        <div className="container-datos bg-primary">
+          <p>dato2</p>
+        </div>
+        <div className="container-datos bg-primary">
+          <p>dato3</p>
+        </div>
+      </div>
+    </>
   )
 }
 
