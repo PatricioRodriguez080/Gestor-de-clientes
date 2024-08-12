0bb9778e7adc10d57d085d02bc51df2f1f4e3e18
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-08-12 20:35:06 -0300

    Mejoras esteticas navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,19 +4,19 @@ import './navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid d-flex align-items-center">
         <Link to={"/"} className="navbar-brand text-light">Inicio</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div className="offcanvas offcanvas-end ms-4 mt-1" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div className="offcanvas-header">
             <h5 className="offcanvas-title text-light" id="offcanvasNavbarLabel">Offcanvas</h5>
             <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <ul className="navbar-nav justify-content-start flex-grow-1 pe-3">
               <li className="nav-item">
                 <Link to={"/clientes"} className="nav-link text-light">Clientes</Link>
               </li>
